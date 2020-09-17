dbb1cdb261c71a8a7a703e5baf879785d3df0973
wodwine <wodwine@hotmail.com>
2020-09-17 23:17:21 +0700

    Latest in first part

---
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -0,0 +1,17 @@
+package th.ac.ku.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Hello there!!");
+        // return home.html
+        return "home";
+    }
+}
+
