d1fba2cee086bd42ef6e2cb7f635e21b5c70d3ec
wodwine <wodwine@hotmail.com>
2020-09-23 22:48:41 +0700

    Connect to JDBC Spring Framework

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import java.util.ArrayList;
@@ -10,25 +12,30 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private ArrayList<Customer> customerList = new ArrayList<>();
+    private CustomerRepository repository;
+
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(customerList);
+        return repository.findAll();
     }
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
     }
+
     public Customer checkPin(Customer inputCustomer) {
         // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
         Customer storedCustomer = findCustomer(inputCustomer.getId());
