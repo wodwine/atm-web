8b9430b1a87d3d61a045de64c106d398e49b7428
wodwine <wodwine@hotmail.com>
2020-09-25 19:08:51 +0700

    Add edit button and connect api with PUT

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -54,5 +54,20 @@ public class BankAccountService {
         return Arrays.asList(accounts);
     }
 
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8081/api/bankaccount/" + id;
+
+        ResponseEntity<BankAccount> response =
+                restTemplate.getForEntity(url, BankAccount.class);
+
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8081/api/bankaccount/" +
+                bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
+
 
 }
